ddfd17da62e596696eea1419ee4c7ce265313516
inordeck <ian.nordeck@gmail.com>
2017-07-07 01:04:45 -0600

    resubmit itterators homework

--- myReduce.js
+++ myReduce.js
@@ -1,10 +1,15 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
+function myReduce(arr, callback, start) {
 
 //  CODE INSIDE HERE   //
-
+		if( typeof start === 'undefined' ) {
+			return arr.reduce(callback);
+			var initVal = start;
+		} else {
+			return arr.reduce(callback, start);
+		}
 }
 
 /*
